7c9455f767d2ef620ceedc5bb38c6f2320201a8b
강재설 <drimsw@gmail.com>
2026-06-29 23:45:05 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-// 2026-06-29 KST | CACHE_NAME v1023 (홈 헤더 레이블 수정)
+// 2026-06-29 KST | CACHE_NAME v1024 (년이월 금액 사이즈 통일)
 'use strict';
-const CACHE_NAME = 'gaegyebu-v1023';
+const CACHE_NAME = 'gaegyebu-v1024';
 const ASSETS = ['./', './index.html', './app.js', './xlsx-js-style.min.js', './manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('message', e => { if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting(); });
